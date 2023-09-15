854f5dfcda5db4249a1edf6b87c906da628cdd3d
Sivasangeeth <sivasangeeth04@gmail.com>
2023-09-15 13:53:09 +0530

    created navbar and modal component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Authen from "./Components/Authen";
 import {database} from "./firebase-config";
 import { useState } from 'react';
+import Navbar from './Components/pages/Navbar';
 function App() {
   const[map,setMap]=useState();
   return (
     <div className="App">
       <Authen/>
-       <p>Hello</p>
-       <h1>World......................</h1>
+      <Navbar/>
     </div>
   );
 }
